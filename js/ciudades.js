e5ecaa4e2d5831bac3ebc04f4217ff8260cb3f56
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2024-10-24 10:30:14 +0200

    feat(#1):create data files js

---
+++ js/ciudades.js
@@ -0,0 +1,43 @@
+listCities = [
+    {
+        id:1,
+        city:"Castellon",
+        coordenates: "39.98567, -0.04935"
+    },{
+        id:2,
+        city:"Valencia",
+        coordenates: "39.47391, -0.37966"
+    },{
+        id:3,
+        city:"Alicante",
+        coordenates: "38.34517, -0.48149"
+    },{
+        id:4,
+        city:"Murcia",
+        coordenates: "37.98704, -1.13004"
+    },{
+        id:5,
+        city:"Madrid",
+        coordenates: "40.4165, -3.70256"
+    },{
+        id:6,
+        city:"Siguenza",
+        coordenates: "41.06892, -2.64308"
+    },{
+        id:7,
+        city:"Zaragoza",
+        coordenates: "41.65606, -0.87734"
+    },{
+        id:8,
+        city:"Lleida",
+        coordenates: "41.61674, 0.62218"
+    },{
+        id:9,
+        city:"Barcelona",
+        coordenates: "41.38879, 2.15899"
+    },{
+        id:9,
+        city:"Utiel",
+        coordenates: "39.564126, -1.193592"
+    }
+]
---
+++ js/empresa.js
@@ -0,0 +1,31 @@
+listEnterprise = [
+    {
+        id:1,
+        name: "Alsa",
+        email: "info@alsa.com",
+        phone: "629384571",
+        logo: "./empresa/alsa.png",
+        web: "alsa.com"
+    },{
+        id:2,
+        name: "Flixbus",
+        email: "info@flixbus.com",
+        phone: "969384671",
+        logo: "./empresa/flixbus.png",
+        web: "flixbus.com"
+    },{
+        id:3,
+        name: "Avanza",
+        email: "info@alsa.com",
+        phone: "969544572",
+        logo: "./empresa/avanza.png",
+        web: "avanza.com"
+    },{
+        id:4,
+        name: "Monbus",
+        email: "info@monbus.com",
+        phone: "639354562",
+        logo: "./empresa/monbus.png",
+        web: "monbus.com"
+    }
+]
